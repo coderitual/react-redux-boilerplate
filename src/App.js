74d4ade7c36b2b8d4b9431d6f1d0852ce3f17137
coderitual <skowronkow@gmail.com>
2018-08-05 15:59:51 +0200

    chore: replace emotion with styled-components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React, { Component } from 'react';
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+  }
+`;
 
 class App extends Component {
   render() {
